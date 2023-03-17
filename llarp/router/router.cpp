8fc4d9f213d2b7edc634f799e533f6052ffd5165
jeflin frank <jeflin.frank@beldex.io>
2023-03-17 13:36:54 +0530

    router status update

--- llarp/router/router.cpp
+++ llarp/router/router.cpp
@@ -862,10 +862,11 @@ namespace llarp
            << NumberOfConnectedRouters() << "/" << NumberOfConnectedClients() << " | "
            << pathContext().CurrentTransitPaths() << " active paths | "
            << "block " << (m_beldexdRpcClient ? m_beldexdRpcClient->BlockHeight() : 0) << " | gossip: "
-           << "(next/last) " << _rcGossiper.NextGossipAt() << "/";
+           << "(next/last) " << time_delta<std::chrono::seconds>{_rcGossiper.NextGossipAt()}
+           << " / ";
         if (auto maybe = _rcGossiper.LastGossipAt())
         {
-          ss << *maybe;
+          ss << time_delta<std::chrono::seconds>{*maybe};
         }
         else
         {
