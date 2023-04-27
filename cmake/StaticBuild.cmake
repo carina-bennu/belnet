6afc9df90f699685e713d595e2591de8a0f49cb2
jeflin frank <jeflin.frank@beldex.io>
2023-04-27 19:39:53 +0530

    mingw patch update

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -331,7 +331,9 @@ endif()
 
 if(CMAKE_CROSSCOMPILING AND ARCH_TRIPLET MATCHES mingw)
   set(zmq_patch
-    PATCH_COMMAND ${PROJECT_SOURCE_DIR}/contrib/apply-patches.sh ${PROJECT_SOURCE_DIR}/contrib/patches/libzmq-mingw-wepoll.patch ${PROJECT_SOURCE_DIR}/contrib/patches/libzmq-mingw-closesocket.patch)
+  PATCH_COMMAND ${PROJECT_SOURCE_DIR}/contrib/apply-patches.sh
+  ${PROJECT_SOURCE_DIR}/contrib/patches/libzmq-mingw-wepoll.patch
+  ${PROJECT_SOURCE_DIR}/contrib/patches/libzmq-mingw-unistd.patch)
 endif()
 
 build_external(zmq
@@ -423,7 +425,6 @@ foreach(curl_arch ${curl_arches})
   list(APPEND curl_lib_outputs ${curl_prefix}/lib/libcurl.a)
 endforeach()
 
-message(STATUS "TARGETS: ${curl_lib_targets}")
 
 if(IOS AND num_arches GREATER 1)
   # We are building multiple architectures for different iOS devices, so we need to glue the
