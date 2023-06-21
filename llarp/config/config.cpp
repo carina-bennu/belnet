dd1537f09670a4f82fd05b8b8a532e8e2f0311ee
jeflin frank <jeflin.frank@beldex.io>
2023-06-21 12:19:57 +0530

    update config description

--- llarp/config/config.cpp
+++ llarp/config/config.cpp
@@ -466,9 +466,8 @@ namespace llarp
         "owned-range",
         MultiValue,
         Comment{
-            "When in exit mode announce we allow a private range in our introset"
-            "exmaple:",
-            "owned-range=10.0.0.0/24",
+            "When in exit mode announce we allow a private range in our introset.  For example:",
+            "    owned-range=10.0.0.0/24",
         },
         [this](std::string arg) {
           IPRange range;
@@ -482,12 +481,17 @@ namespace llarp
         "traffic-whitelist",
         MultiValue,
         Comment{
-            "List of ip traffic whitelist, anything not specified will be dropped by us."
-            "examples:",
-            "tcp for all tcp traffic regardless of port",
-            "0x69 for all packets using ip protocol 0x69"
-            "udp/53 for udp port 53",
-            "tcp/smtp for smtp port",
+            "Adds an IP traffic type whitelist; can be specified multiple times.  If any are",
+            "specified then only matched traffic will be allowed and all other traffic will be",
+            "dropped.  Examples:",
+            "    traffic-whitelist=tcp",
+            "would allow all TCP/IP packets (regardless of port);",
+            "    traffic-whitelist=0x69",
+            "would allow IP traffic with IP protocol 0x69;",
+            "    traffic-whitelist=udp/53",
+            "would allow UDP port 53; and",
+            "    traffic-whitelist=tcp/smtp",
+            "would allow TCP traffic on the standard smtp port (21).",
         },
         [this](std::string arg) {
           if (not m_TrafficPolicy)
@@ -504,9 +508,12 @@ namespace llarp
         MultiValue,
         Comment{
             "Specify a `.bdx` address and an optional ip range to use as an exit broker.",
-            "Example:",
-            "exit-node=whatever.bdx # maps all exit traffic to whatever.bdx",
-            "exit-node=stuff.bdx:100.0.0.0/24 # maps 100.0.0.0/24 to stuff.bdx",
+            "Examples:",
+            "    exit-node=whatever.bdx",
+            "would map all exit traffic through whatever.bdx; and",
+            "    exit-node=stuff.bdx:100.0.0.0/24",
+            "would map the IP range 100.0.0.0/24 through stuff.bdx.",
+            "This option can be specified multiple times (to map different IP ranges).",
         },
         [this](std::string arg) {
           if (arg.empty())
@@ -587,9 +594,11 @@ namespace llarp
         ClientOnly,
         Default{true},
         Comment{
-            "enable / disable auto routing. When using an exit belnet will add routes to "
-            "the OS to make traffic go over the network interface via belnet.",
-            "enabled by default.",
+            "Enable / disable automatic route configuration.",
+            "When this is enabled and an exit is used Belnet will automatically configure the",
+            "operating system routes to route public internet traffic through the exit node.",
+            "This is enabled by default, but can be disabled if advanced/manual exit routing",
+            "configuration is desired."
         },
         AssignmentAcceptor(m_EnableRoutePoker));
 
@@ -601,8 +610,8 @@ namespace llarp
         Default{true},
         Comment{
             "Enable / disable route configuration blackholes.",
-            "When enabled belnet will drop ip4 and ip6 not included in exit config.",
-            "Enabled by default."},
+            "When enabled belnet will drop IPv4 and IPv6 traffic (when in exit mode) that is not",
+            "handled in the exit configuration.  Enabled by default."},
         AssignmentAcceptor(m_BlackholeRoutes));
 
 
@@ -611,7 +620,7 @@ namespace llarp
         "ifname",
         Comment{
             "Interface name for belnet traffic. If unset belnet will look for a free name",
-            "belnetN, starting at 0 (e.g. belnet0, belnet1, ...).",
+            "matching 'belnetN', starting at N=0 (e.g. belnet0, belnet1, ...).",
         },
         AssignmentAcceptor(m_ifname));
 
@@ -635,10 +644,10 @@ namespace llarp
         "ip6-range",
         ClientOnly,
         Comment{
-            "For all ipv6 exit traffic you will use this as the base address bitwised or'd with "
+            "For all IPv6 exit traffic you will use this as the base address bitwised or'd with ",
             "the v4 address in use.",
             "To disable ipv6 set this to an empty value.",
-            "!!! WARNING !!! Disabling ipv6 tunneling when you have ipv6 routes WILL lead to "
+            "!!! WARNING !!! Disabling ipv6 tunneling when you have ipv6 routes WILL lead to ",
             "de-anonymization as belnet will no longer carry your ipv6 traffic.",
         },
         IP6RangeDefault,
@@ -729,9 +738,13 @@ namespace llarp
         ClientOnly,
         MultiValue,
         Comment{
-            "Specify SRV Records for services hosted on the MNApp",
-            "for more info see https://docs.beldex.network/Belnet/Guides/HostingSNApps/",
-            "srv=_service._protocol priority weight port target.beldex",
+            "Specify SRV Records for services hosted on the MNApp for protocols that use SRV",
+            "records for service discovery. Each line specifies a single SRV record as:",
+            "    srv=_service._protocol priority weight port target.bdx",
+            "and can be specified multiple times as needed.",
+            "For more info see",
+            "https://docs.beldex.io/products-built-on-beldex/belnet/mnapps/hosting-mnapps",
+            "and general description of DNS SRV record configuration.",
         },
         [this](std::string arg) {
           llarp::dns::SRVData newSRV;
@@ -746,8 +759,8 @@ namespace llarp
         "path-alignment-timeout",
         ClientOnly,
         Comment{
-            "time in seconds how long to wait for a path to align to pivot routers",
-            "if not provided a sensible default will be used",
+            "How long to wait (in seconds) for a path to align to a pivot router when establishing",
+            "a path through the network to a remote .bdx address.",
         },
         [this](int val) {
           if (val <= 0)
@@ -762,9 +775,10 @@ namespace llarp
         ClientOnly,
         Default{fs::path{params.defaultDataDir / "addrmap.dat"}},
         Comment{
-            "persist mapped ephemeral addresses to a file",
-            "on restart the mappings will be loaded so that ip addresses will not be mapped to a "
-            "different address",
+            "If given this specifies a file in which to record mapped local tunnel addresses so",
+            "the same local address will be used for the same belnet address on reboot.  If this",
+            "is not specified then the local IP of remote belnet targets will not persist across",
+            "restarts of belnet.",
         },
         [this](fs::path arg) {
           if (arg.empty())
@@ -887,7 +901,7 @@ namespace llarp
             "(This is not used directly by belnet itself, but by the belnet init scripts",
             "on systems which use resolveconf)",
         });
-        // forwad the rest to libunbound
+        // forward the rest to libunbound
     conf.addUndeclaredHandler("dns", [this](auto, std::string_view key, std::string_view val) {
       m_ExtraOpts.emplace(key, val);
     });
@@ -1178,7 +1192,7 @@ namespace llarp
         "rpc",
         RelayOnly,
         Comment{
-            "lokiMQ control address for for communicating with beldexd. Depends on beldexd's",
+            "oxenmq control address for for communicating with beldexd. Depends on beldexd's",
             "lmq-local-control configuration option. By default this value should be",
             "ipc://BELDEXD-DATA-DIRECTORY/beldexd.sock, such as:",
             "    rpc=ipc:///var/lib/beldex/beldexd.sock",
@@ -1212,7 +1226,7 @@ namespace llarp
         "add-node",
         MultiValue,
         Comment{
-            "Specify a bootstrap file containing a signed RouterContact of a master node",
+            "Specify a bootstrap file containing a list of signed RouterContacts of master nodes",
             "which can act as a bootstrap. Can be specified multiple times.",
         },
         [this](std::string arg) {
@@ -1301,9 +1315,9 @@ namespace llarp
           m_UniqueHopsNetmaskSize = arg;
         },
         Comment{
-            "Netmask for router path selection; each router must be from a distinct IP subnet "
+            "Netmask for router path selection; each router must be from a distinct IPv4 subnet",
             "of the given size.",
-            "E.g. 16 ensures that all routers are using distinct /16 IP addresses."});
+            "E.g. 16 ensures that all routers are using IPs from distinct /16 IP ranges."});
 
 #ifdef WITH_GEOIP
     conf.defineOption<std::string>(
@@ -1315,9 +1329,11 @@ namespace llarp
           m_ExcludeCountries.emplace(lowercase_ascii_string(std::move(arg)));
         },
         Comment{
-            "exclude a country given its 2 letter country code from being used in path builds",
-            "e.g. exclude-country=DE",
-            "can be listed multiple times to exclude multiple countries"});
+            "Exclude a country given its 2 letter country code from being used in path builds.",
+            "For example:",
+            "    exclude-country=DE",
+            "would avoid building paths through routers with IPs in Germany.",
+            "This option can be specified multiple times to exclude multiple countries"});
 #endif
   }
 
@@ -1609,7 +1625,7 @@ namespace llarp
     def.addSectionComments(
         "network",
         {
-            "Snapp settings",
+            "Mnapp settings",
         });
 
     return def.generateINIConfig(true);
